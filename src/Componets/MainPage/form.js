5305b2c20fcc46bb7d6b725c4d4aa685716a2c47
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2022-04-19 16:50:27 +0300

    fead: add style in h1

--- src/Componets/MainPage/form.js
+++ src/Componets/MainPage/form.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Form = props => (
     <form onSubmit={props.weatherMethod}>
       <input type="text" name="city" placeholder="Город"/>
-      <button>Получить погоду</button>
+      <button>Получить информацию</button>
     </form>
   )
 
