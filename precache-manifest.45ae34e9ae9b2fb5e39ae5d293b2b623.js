c80cac9881a0911d6ea31d4719290c8471677b78
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2022-06-09 18:27:00 +0300

    Updates

---
+++ precache-manifest.45ae34e9ae9b2fb5e39ae5d293b2b623.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c5d99a812ec40eaadb3e397cfe65ac",
+    "url": "/ITgenio/index.html"
+  },
+  {
+    "revision": "1f034603d2cd2d685439",
+    "url": "/ITgenio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "77562121d742f4045354",
+    "url": "/ITgenio/static/css/main.d01a120d.chunk.css"
+  },
+  {
+    "revision": "1f034603d2cd2d685439",
+    "url": "/ITgenio/static/js/2.986cc91c.chunk.js"
+  },
+  {
+    "revision": "9c093a58bd47bb7f5206",
+    "url": "/ITgenio/static/js/3.246faef4.chunk.js"
+  },
+  {
+    "revision": "77562121d742f4045354",
+    "url": "/ITgenio/static/js/main.2c534a9a.chunk.js"
+  },
+  {
+    "revision": "e31fa1091637d1db312e",
+    "url": "/ITgenio/static/js/runtime~main.7b3eca27.js"
+  },
+  {
+    "revision": "8e3fccee259aa5a7efc7f08e6dfb8c3a",
+    "url": "/ITgenio/static/media/0.8e3fccee.jpg"
+  },
+  {
+    "revision": "4048b5d26935de84c29bb4c9e0f2d960",
+    "url": "/ITgenio/static/media/1.4048b5d2.jpg"
+  },
+  {
+    "revision": "9b844f48878625675a49191a798323f9",
+    "url": "/ITgenio/static/media/10.9b844f48.jpg"
+  },
+  {
+    "revision": "a831c4cece5b104c2ceba57c85af48bc",
+    "url": "/ITgenio/static/media/11.a831c4ce.jpg"
+  },
+  {
+    "revision": "2f1f67efabe5155b293ee14d90c3cb5d",
+    "url": "/ITgenio/static/media/12.2f1f67ef.jpg"
+  },
+  {
+    "revision": "89ae9ffa4a962b8f179117c906d640e3",
+    "url": "/ITgenio/static/media/13.89ae9ffa.jpg"
+  },
+  {
+    "revision": "57120a0edc519e86531ae4f29fa400d6",
+    "url": "/ITgenio/static/media/14.57120a0e.jpg"
+  },
+  {
+    "revision": "a50fca97191f816e2ee21066258cc1b6",
+    "url": "/ITgenio/static/media/15.a50fca97.jpg"
+  },
+  {
+    "revision": "4e202cdb606d7fa29b81d32d019815cf",
+    "url": "/ITgenio/static/media/16.4e202cdb.jpg"
+  },
+  {
+    "revision": "200a8dbcec06466e44d2f4de7caa0b01",
+    "url": "/ITgenio/static/media/17.200a8dbc.jpg"
+  },
+  {
+    "revision": "2395e42fcd09df8536f218b36d8983c4",
+    "url": "/ITgenio/static/media/18.2395e42f.jpg"
+  },
+  {
+    "revision": "3bfc19934f831cf801c24e254e3725f5",
+    "url": "/ITgenio/static/media/19.3bfc1993.jpg"
+  },
+  {
+    "revision": "d3aa82451175c5fad6ee9522d2eecf2b",
+    "url": "/ITgenio/static/media/2.d3aa8245.jpg"
+  },
+  {
+    "revision": "aefdcf08f7f3d5974e2b98c2f866148a",
+    "url": "/ITgenio/static/media/20.aefdcf08.jpg"
+  },
+  {
+    "revision": "68bea40efcb3929cc902bdc4d179ad37",
+    "url": "/ITgenio/static/media/21.68bea40e.jpg"
+  },
+  {
+    "revision": "f1c79710264e7c1f5f8e663a4c25d5fc",
+    "url": "/ITgenio/static/media/22.f1c79710.jpg"
+  },
+  {
+    "revision": "6674d9a7bbe624b0d94817a1f7aef3b7",
+    "url": "/ITgenio/static/media/23.6674d9a7.jpg"
+  },
+  {
+    "revision": "247407b705d6ed6ac666bea166987d42",
+    "url": "/ITgenio/static/media/3.247407b7.jpg"
+  },
+  {
+    "revision": "e810a40fceb755c1334c2d452f7a5640",
+    "url": "/ITgenio/static/media/4.e810a40f.jpg"
+  },
+  {
+    "revision": "af09cc63f703790f33c2822f6a68314f",
+    "url": "/ITgenio/static/media/5.af09cc63.jpg"
+  },
+  {
+    "revision": "9d80a2457700b32c62659199a776c4ca",
+    "url": "/ITgenio/static/media/6.9d80a245.jpg"
+  },
+  {
+    "revision": "9021cf846d6583bbe42de4fd38610e71",
+    "url": "/ITgenio/static/media/7.9021cf84.jpg"
+  },
+  {
+    "revision": "2787c7276c489e0dfdf403acc97d8070",
+    "url": "/ITgenio/static/media/8.2787c727.jpg"
+  },
+  {
+    "revision": "ed6fc55cbe2a34d49bb29d68e57fbc98",
+    "url": "/ITgenio/static/media/9.ed6fc55c.jpg"
+  },
+  {
+    "revision": "a000042de8ffc0daa6c7e8e356f323dc",
+    "url": "/ITgenio/static/media/itgenik-02.a000042d.png"
+  }
+]);
